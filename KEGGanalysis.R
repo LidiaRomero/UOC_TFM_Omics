7de318e4749f1b778346bd390d7da04efc4b8767
LidiaRomero <151547895+LidiaRomero@users.noreply.github.com>
2023-12-11 10:10:18 +0100

    Delete KEGGanalysis.R

--- KEGGanalysis.R
+++
@@ -1,9 +0,0 @@
-KEGGanalysis <- function(matS){
-  signif_GeneID <- matS$GeneID
-  KEGGparams <- enrichKEGG(gene = signif_GeneID,
-                           organism = "hsa",
-                           pvalueCutoff = 0.05) 
-  return(KEGGparams)
-}
-
-
